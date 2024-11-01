3fb11e852712ac4bf51faafeae9901458eff2826
AntolinoRetana <antolinor8@gmail.com>
2024-10-31 21:21:18 -0600

    Migraciones notificaciones y estudiantes

---
+++ database/migrations/2024_11_01_025712_create_notificaciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notificaciones', function (Blueprint $table) {
+            $table->id('id_notificacion');
+            $table->foreignId('id_usuario')->constrained('usuarios');
+            $table->text('mensaje');
+            $table->string('estado');
+            $table->date('fecha_envio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notificaciones');
+    }
+};
