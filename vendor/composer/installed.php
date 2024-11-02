e54105a3fe3dc57cf48b869f09dcaf899aa231fe
yoselinuesfom <jo21003@ues.edu.sv>
2024-11-01 23:07:36 -0600

    Eliminacion de la tabla Usuarios

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,11 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7180d8f02285f9fad873a4e25e3a0b2d9cfaf7aa',
+<<<<<<< HEAD
+        'reference' => '01c6d18467a93b1772532fbca8640cbfab042c8d',
+=======
+        'reference' => 'd23d61a507f47970eb40f99a307800def6a9601b',
+>>>>>>> b8923f3e6dfc20a76acc58a5c2bb05dea2168b33
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -388,7 +392,11 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7180d8f02285f9fad873a4e25e3a0b2d9cfaf7aa',
+<<<<<<< HEAD
+            'reference' => '01c6d18467a93b1772532fbca8640cbfab042c8d',
+=======
+            'reference' => 'd23d61a507f47970eb40f99a307800def6a9601b',
+>>>>>>> b8923f3e6dfc20a76acc58a5c2bb05dea2168b33
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -958,6 +966,15 @@
             'install_path' => __DIR__ . '/../sebastian/version',
             'aliases' => array(),
             'dev_requirement' => true,
+        ),
+        'spatie/laravel-permission' => array(
+            'pretty_version' => '6.9.0',
+            'version' => '6.9.0.0',
+            'reference' => 'fe973a58b44380d0e8620107259b7bda22f70408',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../spatie/laravel-permission',
+            'aliases' => array(),
+            'dev_requirement' => false,
         ),
         'spatie/once' => array(
             'dev_requirement' => false,
