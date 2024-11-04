5ecee9a003c72ea049705f21aac226752300ff11
marvinJosueBatresRivas <br20028@ues.edu.sv>
2024-11-03 19:29:47 -0600

    cambios tabla estado

--- database/migrations/2024_11_04_012355_create_estados_table.php
+++ database/migrations/2024_11_04_012355_create_estados_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('estados', function (Blueprint $table) {
             $table->id();
+            $table->text('nombre_estados');
             $table->timestamps();
         });
     }
