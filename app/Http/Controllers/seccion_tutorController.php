5f49737ff801adf1e6161869df10ad59130308a3
Josué04R <ro22024@ues.edu.sv>
2024-11-21 12:20:51 -0600

    subiendo el model seccionn_tutor y controller

---
+++ app/Http/Controllers/seccion_tutorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\seccion_tutor;
+use Illuminate\Http\Request;
+
+class seccion_tutorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(seccion_tutor $seccion_tutor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(seccion_tutor $seccion_tutor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, seccion_tutor $seccion_tutor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(seccion_tutor $seccion_tutor)
+    {
+        //
+    }
+}
