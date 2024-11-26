8b10adcc4038a10d8297dfbdfe2d485d397a5d2b
0kev0 <117107399+0kev0@users.noreply.github.com>
2024-11-26 13:42:22 -0600

    correcciones varias
    - cambio a funcion Detalles_proyecto() para integrar la barra de porcentaje
    
    - ajuste en estilo del Titulo del navbar
    
    -cambio en blade mensajes para que el chat no tome mas espacio de 100vh
    
    - ajuste en limite de horas que se pueden solicitar en blade actualizar-horas
    
    -

--- resources/views/estudiantes/detallesmio.blade.php
+++ resources/views/estudiantes/detallesmio.blade.php
@@ -16,78 +16,91 @@
     <div class="card-body">
 
       <h2>{{ $proyectoEstudiante->proyecto->nombre_proyecto }}</h2>
-
       <!-- Progress bar -->
       <div class="my-4">
         @php
-        $porcentaje = optional($proyectoEstudiante)->porcentaje_completado ?? 0;
+        $progressClass = 'bg-danger';
+        if ($porcentaje > 25) $progressClass = 'bg-warning';
+        if ($porcentaje > 50) $progressClass = 'bg-info';
+        if ($porcentaje > 75) $progressClass = 'bg-success';
         @endphp
-        <div class="d-flex justify-content-between ">
-          <p class="card-text">Progreso del Proyecto</p>
-          <p>Completadas {{ $proyectoEstudiante->horas_sociales_completadas ?? 0 }} de {{ $proyectoEstudiante->proyecto->horas_requeridas }} horas</p>
-        </div>
 
-        <div class="progress" style="height: 20px;">
-          <div
-            class="progress-bar"
-            role="progressbar"
-            style="width: {{ $porcentaje }}%;"
-            aria-valuenow="{{ $porcentaje }}"
-            aria-valuemin="0"
-            aria-valuemax="100">
-            {{ $porcentaje }}%
+        <div class="my-4">
+          @php
+          $porcentaje = $porcentaje ?? 0;
+          @endphp
+          <div class="d-flex justify-content-between ">
+            <p class="card-text">Progreso del Proyecto</p>
+            <p class="text-muted">
+              @if ($horasTotales == 0)
+              Horas del proyecto pendientes de asignar.
+              @else
+              Completadas {{ $horasCompletadas }} de {{ $horasTotales }} horas.
+              @endif
+            </p>
           </div>
+
+          <div class="progress" style="height: 25px;">
+            <div
+              class="progress-bar {{ $progressClass }} progress-bar-striped progress-bar-animated"
+              role="progressbar"
+              style="width: {{ $porcentaje }}%;"
+              aria-valuenow="{{ $porcentaje }}"
+              aria-valuemin="0"
+              aria-valuemax="100">
+              {{ $porcentaje }}%
+            </div>
+          </div>
+
         </div>
-        <h3>{{$porcentaje}} % </h3>
-      </div>
 
-      <!-- Project details -->
-      <div class="mr-4">
-        <p class="card-text">
-          <i class="bi bi-calendar"></i>
-          Inicio: {{ \Carbon\Carbon::parse($proyectoEstudiante->proyecto->fecha_inicio)->translatedFormat('d F Y') }}
-        </p>
-        <p class="card-text">
-          <i class="bi bi-calendar-event"></i>
-          Fin: {{ \Carbon\Carbon::parse($proyectoEstudiante->proyecto->fecha_fin)->translatedFormat('d F Y') }}
-        </p>
+        <!-- Project details -->
+        <div class="mr-4">
+          <p class="card-text">
+            <i class="bi bi-calendar"></i>
+            Inicio: {{ \Carbon\Carbon::parse($proyectoEstudiante->proyecto->fecha_inicio)->translatedFormat('d F Y') }}
+          </p>
+          <p class="card-text">
+            <i class="bi bi-calendar-event"></i>
+            Fin: {{ \Carbon\Carbon::parse($proyectoEstudiante->proyecto->fecha_fin)->translatedFormat('d F Y') }}
+          </p>
 
-        <p class="card-text"><i class="bi bi-geo-alt-fill"></i></i>{{ $proyectoEstudiante->proyecto->lugar }}</p>
-        <p class="card-text"><i class="bi bi-person-fill"></i>Tutor: {{ $proyectoEstudiante->proyecto->tutorr ? $proyectoEstudiante->proyecto->tutorr->name : 'sin asignar' }}</p>
-        <button class="btn-verde btn m-3">{{ $proyectoEstudiante->proyecto->estadoos->nombre_estado }}</button>
+          <p class="card-text"><i class="bi bi-geo-alt-fill"></i></i>{{ $proyectoEstudiante->proyecto->lugar }}</p>
+          <p class="card-text"><i class="bi bi-person-fill"></i>Tutor: {{ $proyectoEstudiante->proyecto->tutorr ? $proyectoEstudiante->proyecto->tutorr->name : 'sin asignar' }}</p>
+          <button class="btn-verde btn m-3">{{ $proyectoEstudiante->proyecto->estadoos->nombre_estado }}</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<div class="container containerhistorial mt-4">
-  <div class="card shadow-m">
-    <div class="card-body">
-      <h2>Historial de actualizaciones</h2>
-      <div class="table-responsive">
-        <table class="table table-borderless">
-          <thead>
-            <tr>
-              <th>Fecha</th>
-              <th>Horas</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>4 de marzo de 2024</td>
-              <td>5</td>
-            </tr>
-            <tr>
-              <td>11 de marzo de 2024</td>
-              <td>7</td>
-            </tr>
-          </tbody>
-        </table>
+  <div class="container containerhistorial mt-4">
+    <div class="card shadow-m">
+      <div class="card-body">
+        <h2>Historial de actualizaciones</h2>
+        <div class="table-responsive">
+          <table class="table table-borderless">
+            <thead>
+              <tr>
+                <th>Fecha</th>
+                <th>Horas</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>4 de marzo de 2024</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>11 de marzo de 2024</td>
+                <td>7</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
+      <p class="text-center text-muted opacity-50">Registro de horas trabajadas</p>
     </div>
-    <p class="text-center text-muted opacity-50">Registro de horas trabajadas</p>
   </div>
-</div>
 
 
-@endsection
+  @endsection
